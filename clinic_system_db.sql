3a07e0ce2c2cd84daf61467a9079ab085c050523
zd2U09 <zd2U09@zd2U09>
2026-05-20 13:47:23 +0900

    ClinicSystem開発開始

---
+++ clinic_system_db.sql
@@ -0,0 +1,39 @@
+CREATE SCHEMA `clinic_system_db` ;
+CREATE TABLE `clinic_system_db`.`admins` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `password_hash` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+CREATE TABLE `clinic_system_db`.`customers` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `phone_number` VARCHAR(11) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `birth_date` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idcustomers_UNIQUE` (`id` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `phone_number_UNIQUE` (`phone_number` ASC) VISIBLE);
+  CREATE TABLE `clinic_system_db`.`reservations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `customer_id` INT NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `reserved_at` DATETIME NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idreservations_UNIQUE` (`id` ASC) VISIBLE);
+ALTER TABLE `clinic_system_db`.`admins` 
+CHANGE COLUMN `password_hash` `password_hash` VARCHAR(255) NOT NULL ;
+ALTER TABLE `clinic_system_db`.`customers` 
+CHANGE COLUMN `phone_number` `phone_number` VARCHAR(20) NOT NULL ;
+ALTER TABLE `clinic_system_db`.`reservations` 
+CHANGE COLUMN `created_at` `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ;
+
+INSERT INTO `clinic_system_db`.`customers` 
+    (`name`, `phone_number`, `email`, `birth_date`) 
+VALUES 
+    ('高巻杏', '09011111111', 'an@gmail.com', '2000-11-12'),
+    ('新島真', '09022222222', 'makoto@gmail.com', '2000-04-23'),
+    ('佐倉双', '09033333333', 'futaba@gmail.com', '2000-02-19'),
+    ('奥村春', '09044444444', 'haru@gmail.com', '2000-12-05'),
+    ('芳澤かすみ', '09055555555', 'kasumi@gmail.com', '2000-03-25');
---
+++ src/main/java/com/example/app/ClinicSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicSystemApplication.class, args);
+	}
+
+}
